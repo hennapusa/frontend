ed5d6e2d7e74e21b73a87ec2bc41c5cbd82ff513
hennapusa <77694207+hennapusa@users.noreply.github.com>
2021-11-12 09:17:04 +0200

    pööö

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TestiComponent } from './testi/testi.component';
+import {MatButtonModule} from "@angular/material/button";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import { CalculatorComponent } from './calculator/calculator.component';
+import {MatGridListModule} from "@angular/material/grid-list";
+import { FeedbackComponent } from './feedback/feedback.component';
+import {MatSliderModule} from "@angular/material/slider";
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestiComponent,
+    CalculatorComponent,
+    FeedbackComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+    MatGridListModule,
+    MatSliderModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
