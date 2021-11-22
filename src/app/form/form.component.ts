f642f25b234876fe59a6fc80039bc9a2aa3a92db
hennapusa <77694207+hennapusa@users.noreply.github.com>
2021-11-22 13:02:23 +0200

    tallenna

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {Person} from "../person";
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+  person: Person;
+
+
+  constructor() {
+  this.person = new Person();
+
+  }
+
+  ngOnInit(): void {
+  }
+
+
+
+}
---
+++ src/app/person.ts
@@ -0,0 +1,21 @@
+export class Person {
+
+  firstName: any;
+  lastName: any;
+email: any;
+password: any;
+id: any;
+userName: any;
+
+
+  constructor() {
+    this.firstName='';
+    this.lastName='';
+    this.email='';
+    this.password='';
+    this.id='';
+    this.userName='';
+
+  }
+}
+
